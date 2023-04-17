6dcff33286d331a66086e1c1e61545f8de5de077
jayceee1207 <ablayjc7@gmail.com>
2023-04-17 11:28:58 +0800

    Corrected indentation

--- vigenere_cipher.py
+++ vigenere_cipher.py
@@ -25,89 +25,89 @@ while moredata == "yes":
     result = pyfiglet.figlet_format("VIGENERE CIPHER", font = "bubble" )
     print(result)
 #Enumerate all the ASCII value and will be inserted inside the empty dictionary
-for i, char in enumerate(string.ascii_uppercase):
-    dict[i] = char
+    for i, char in enumerate(string.ascii_uppercase):
+        dict[i] = char
 
 #Make a function named loop_key to get the list of key
-def loop_key(message, key):
-        key = list(key)
-        if len(message) == len(key):
-            return key
-        else:
-            for i in range(len(message) - len(key)):
-                key.append(key[i % len(key)])
-        return "".join(key)
+    def loop_key(message, key):
+            key = list(key)
+            if len(message) == len(key):
+                return key
+            else:
+                for i in range(len(message) - len(key)):
+                    key.append(key[i % len(key)])
+            return "".join(key)
 
 #Make a function named cipher_txt to get the list of cipher text
-def cipher_txt(message, key):
-        cipher_text = []
-        for i in range(len(message)):
-            x = (ord(message[i]) + ord(key[i])) % 26
-            x += ord('A')
-            cipher_text.append(chr(x))
-        return " ".join(cipher_text)
+    def cipher_txt(message, key):
+            cipher_text = []
+            for i in range(len(message)):
+                x = (ord(message[i]) + ord(key[i])) % 26
+                x += ord('A')
+                cipher_text.append(chr(x))
+            return " ".join(cipher_text)
 
 #Make a function named add_bot to get the sum of the values of each paired characters
-def add_bot(message, key):
-        add = []
-        for i in range(len(message)):
-            x = (ord(message[i])-65) + (ord(key[i])-65)
-            add.append(x)
-        print("Add: ", add)
-        return
+    def add_bot(message, key):
+            add = []
+            for i in range(len(message)):
+                x = (ord(message[i])-65) + (ord(key[i])-65)
+                add.append(x)
+            print("Add: ", add)
+            return
     
 #Make a function named mod_bot to encrypt the message inputted by the user
-def mod_bot(message, key):
-        mod = []
-        for i in range(len(message)):
-            x = ((ord(message[i]) - 65) + (ord(key[i]) - 65)) % 26
-            mod.append(x)
-        print("Mod: ", mod)
-        return
+    def mod_bot(message, key):
+            mod = []
+            for i in range(len(message)):
+                x = ((ord(message[i]) - 65) + (ord(key[i]) - 65)) % 26
+                mod.append(x)
+            print("Mod: ", mod)
+            return
 
 #Ask the user for their input
-message = str(input("Message: "))
+    message = str(input("Message: "))
 
 #Ask the user for their keyword
-keyword = str(input("Key: "))
+    keyword = str(input("Key: "))
 
 #Print the key
-key = loop_key(message, keyword)
+    key = loop_key(message, keyword)
 #Print the Ciphertext
-cipher_text = cipher_txt(message, key)
+    cipher_text = cipher_txt(message, key)
 
 #Make an empty variable for num_message and num_key
-num_message = ""
-num_key = ""
+    num_message = ""
+    num_key = ""
 
 #Use for loop to get the number of base of each character of message in the dictionary
-for char in message:
-        for num, letter in dict.items():
-            if char == letter:
-                num_message += str(num)
-                num_message += " "
+    for char in message:
+            for num, letter in dict.items():
+                if char == letter:
+                    num_message += str(num)
+                    num_message += " "
 #Use for loop to get the number of base of each character of keyword in the dictionary
-for char in key:
-        for num, letter in dict.items():
-            if char == letter:
-                num_key += str(num)
-                num_key += " "
+    for char in key:
+            for num, letter in dict.items():
+                if char == letter:
+                    num_key += str(num)
+                    num_key += " "
 #Print the Message
-print("\u001b[33;1m","Message: ", message, num_message)
+    print("\u001b[33;1m","Message: ", message, num_message)
 
 #Print the Keyword
-print("\u001b[33;1m","Key: ", keyword, num_key)
+    print("\u001b[33;1m","Key: ", keyword, num_key)
 
-sum = add_bot(message, key)
-mod = mod_bot(message, key)
+    sum = add_bot(message, key)
+    mod = mod_bot(message, key)
 
 #Print the Sum
-print("\u001b[32;1m","Add: ", sum) 
+    print("\u001b[32;1m","Add: ", sum) 
     
 #Print the Mod 
-print("\u001b[32;1m","Mod: ", mod)
+    print("\u001b[32;1m","Mod: ", mod)
 #Print the Cipthertext
-print("\u001b[37;1m","Ciphertext", cipher_text)
+    print("\u001b[37;1m","Ciphertext", cipher_text)
 
 #Ask the user if they want to use the program again
 #   if true
