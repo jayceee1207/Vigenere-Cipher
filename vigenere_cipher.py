76dcf5a176dabdd4587087099849ec0677ce8f6a
jayceee1207 <ablayjc7@gmail.com>
2023-04-17 10:24:21 +0800

    Pseudocode for the Vigenere Cipher Program

--- vigenere_cipher.py
+++ vigenere_cipher.py
@@ -6,7 +6,33 @@ author_name = ("PROGRAMMED BY: JOHN CARLO ABLAY")
 name = author_name.center(100)
 print(name)
 
-#this variable won't be used in the program, this will just be my guide for the letter and its corresponding value.
+
+#PSEUDOCODE
+#Make a dictionary of every letter with it corresponding base value.
+#this variable won't be used in the program, this will just be my guide for the letter and its corresponding base value.
 dict_made = {'A' : 0, 'B' : 1, 'C' : 2, 'D' : 3, 'E' : 4, 'F' : 5, 'G' : 6, 'H' : 7, 'I' : 8, 'J' : 9, 'K' : 10,
             'L' : 11, 'M' : 12, 'N' : 13, 'O' : 14, 'P' : 15, 'Q' : 16, 'R' : 17, 'S' : 18, 'T' : 19, 'U' : 20, 'V' : 21,  'W' : 22,
             'X' : 23, 'Y' : 24, 'Z' : 25}
+#Make a variable with True value to ask the user if they want to try the program again 
+#Title of Program using pyfiglet
+#Enumerate all the ASCII value and will be inserted inside the empty dictionary
+#Make a function named loop_key to get the list of key
+#Make a function named cipher_txt to get the list of cipher text
+#Make a function named add_bot to get the sum of the values of each character
+#Make a function named mod_bot to decrypt the message inputted by the user
+#Ask the user for their input
+#Ask the user for their keyword
+#Print the key
+#Print the Ciphertext
+#Make an empty variable for num_message and num_key
+#Use for loop to get the number of base of each character of message in the dictionary
+#Use for loop to get the number of base of each character of keyword in the dictionary
+#Print the Message
+#Print the Keyword
+#Print the Sum
+#Print the Mod 
+#Print the Cipthertext
+#Ask the user if they want to use the program again
+#   if true
+#       Run the program again
+#   End the program with an exit message
