d030c90596e1ff0dac95482be91131c37ea0fcb7
jayceee1207 <ablayjc7@gmail.com>
2023-04-17 10:12:17 +0800

    Make a dictionary of each character with its corresponding base number

--- vigenere_cipher.py
+++ vigenere_cipher.py
@@ -5,3 +5,8 @@
 author_name = ("PROGRAMMED BY: JOHN CARLO ABLAY")
 name = author_name.center(100)
 print(name)
+
+#this variable won't be used in the program, this will just be my guide for the letter and its corresponding value.
+dict_made = {'A' : 0, 'B' : 1, 'C' : 2, 'D' : 3, 'E' : 4, 'F' : 5, 'G' : 6, 'H' : 7, 'I' : 8, 'J' : 9, 'K' : 10,
+            'L' : 11, 'M' : 12, 'N' : 13, 'O' : 14, 'P' : 15, 'Q' : 16, 'R' : 17, 'S' : 18, 'T' : 19, 'U' : 20, 'V' : 21,  'W' : 22,
+            'X' : 23, 'Y' : 24, 'Z' : 25}
